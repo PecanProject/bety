00f89e4bf06504a8fbd020b7f72307f2859350e0
Patrick Mulrooney <mulroony@gmail.com>
2012-05-10 15:04:25 -0500

    add default order to coordinate_search query

--- app/models/site.rb
+++ app/models/site.rb
@@ -10,9 +10,13 @@ class Site < ActiveRecord::Base
 
   named_scope :all_order, :order => 'country, state, city'
 
+    #20 miles
+    #lat ~ miles/69.1
+    #lng ~ miles/53.0
   named_scope :coordinate_search, lambda { |lat,lon,radius| { :conditions => { 
                                                                 :lat => (lat-(radius/69.1))..(lat+(radius/69.1)),
-                                                                :lon => (lon-(radius/53.0))..(lon+(radius/53.0)) } } }
+                                                                :lon => (lon-(radius/53.0))..(lon+(radius/53.0)) },
+                                                              :order => "country, state, city" } }
 
   comma do
     id
