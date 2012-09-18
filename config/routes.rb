f38c1349998f44338f1726b92fdc67946fab250b
Patrick Mulrooney <mulroony@gmail.com>
2012-09-18 14:17:21 -0500

    fix links in template to mkae them work between suburl and no suburl, for easier coding

--- config/routes.rb
+++ config/routes.rb
@@ -115,7 +115,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  #map.root '/'
 
   # See how all your routes lay out with "rake routes"
 
@@ -125,6 +125,9 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action.:format'
+
+
+  map.root :controller => 'static'
 
 
   #route for 'static' content
