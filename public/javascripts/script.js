2c68a1c20d76a3d19ba4a589046fbedeace5f973
Carl Crott <carlcrott@gmail.com>
2012-09-24 19:08:39 -0400

    added all necessary css files

---
+++ public/javascripts/script.js
@@ -0,0 +1,21 @@
+/* Author:
+
+*/
+/*
+
+$(document).ready(function(){
+	//$('html').addClass('touch');
+        $('ul.main-menu').pufferfish();
+	// Run Mathias Bynens jQuery placeholder plugin (see plugins.js)
+	if ($.fn.placeholder) {
+		$('input, textarea').placeholder();		
+	}
+        
+});
+
+
+
+
+*/
+
+
