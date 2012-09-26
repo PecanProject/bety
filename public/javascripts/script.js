38993695a4de91c3044c4ebb80bfcffbdca9e484
Carl Crott <carlcrott@gmail.com>
2012-09-25 22:39:17 -0400

    #1328 fixed up a few misc bugs

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,31 @@
 /* Author:
 
 */
-/*
 
-$(document).ready(function(){
+
+jQuery(document).ready(function(){
+
+  if ( jQuery('#flash_notice').is(":visible") ) {
+    jQuery('#flash_notice').delay(1000).fadeOut('slow');
+  }
+
+  
+
 	//$('html').addClass('touch');
-        $('ul.main-menu').pufferfish();
+  //  $('ul.main-menu').pufferfish();
 	// Run Mathias Bynens jQuery placeholder plugin (see plugins.js)
-	if ($.fn.placeholder) {
-		$('input, textarea').placeholder();		
-	}
+  //	if ($.fn.placeholder) {
+  //		$('input, textarea').placeholder();		
+  //	}
+        
+        
+        
+        
         
 });
 
 
 
 
-*/
 
 
