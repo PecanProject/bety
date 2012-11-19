0c65beefd28ab0e5be5da257e51e486a4236df4b
Carl Crott <carlcrott@gmail.com>
2012-11-19 10:05:24 -0600

    #1439 and #1128 final migration edits for functional workflows

--- db/migrate/20121108152216_add_model_type_to_models.rb
+++ db/migrate/20121108152216_add_model_type_to_models.rb
@@ -4,9 +4,6 @@ class AddModelTypeToModels < ActiveRecord::Migration
 
   def self.up
     add_column :models, :model_type, :string
-    
-    Model.update_all("model_type = 'ED2'", "model_name like 'ed%'")
-    Model.update_all("model_type = 'SIPNET'", "model_name like 'sipnet%'")
 
     Models.update_all("model_type = 'ED2'", "model_name like 'ed%'")
     Models.update_all("model_type = 'SIPNET'", "model_name like 'sipnet%'")
