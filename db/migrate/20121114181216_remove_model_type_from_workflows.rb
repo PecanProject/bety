94ccbb4945c80282622b8cab32ffddf2d0db9c3d
Carl Crott <carlcrott@gmail.com>
2012-11-19 10:08:54 -0600

    removing another unnecessary migration

--- db/migrate/20121114181216_remove_model_type_from_workflows.rb
+++
@@ -1,9 +0,0 @@
-class RemoveModelTypeFromWorkflows < ActiveRecord::Migration
-  def self.up
-    remove_column :workflows, :model_type
-  end
-
-  def self.down
-    add_column :workflows, :model_type
-  end
-end
