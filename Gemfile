dc1f0367855fc6730eb18152534db81bc0d5542c
Andrew Shirk <shirk@illinois.edu>
2012-11-20 11:53:29 -0600

    Getting BETY to run locally

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "http://rubygems.org"
 gem "rails", "2.3.11"
 
-gem "mysql"
+gem "mysql2"
 gem "activerecord-mysql2-adapter"
 #gem "rspec", "2.11.0"
 
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
   Recaptcha.configure do |config|
-    config.public_key  = '6Ld1vMISAAAAAHhuRlfmrZjSWQV9G7CY9dU0XfMz'
-    config.private_key = '6Ld1vMISAAAAADKIw0RRC3cLU9tfyoSyCte5Llro'
+    config.public_key  = '6Ld5M9kSAAAAAK7eXugwXriDcyIOvl3V1NVyiXl9'
+    config.private_key = '6Ld5M9kSAAAAAFBmB5WmUHoyLIcKDxAasvFp_uCz'
   end
