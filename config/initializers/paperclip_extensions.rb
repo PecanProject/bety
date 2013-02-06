329cf27d12ae7afcb04257b1acf7c00917ae9969
Carl Crott <carlcrott@gmail.com>
2013-02-06 12:04:05 -0500

    resolving issues with session creation

--- config/initializers/paperclip_extensions.rb
+++
@@ -1,6 +0,0 @@
-#From: http://mediumexposure.com/set-paperclip-use-hashed-file-paths/
-
-# RAILS3 changed syntax for newest version of paperclip
-Paperclip.interpolates :hashed_path do |attachment, style|
-  attachment.instance.md5
-end
