6719d94508e8c9666cedd2bb5758c4ac71095036
Carl Crott <carlcrott@gmail.com>
2013-02-20 19:17:12 -0800

    verified tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,6 +8,31 @@ describe SessionsController do
       response.should be_success
     end
   end
+  
+
+
+  describe "invalid signin" do
+
+    before(:each) do
+      @attr = { :email => "email@example.com", :password => "invalid" }
+    end
+
+    it "should re-render the new page" do
+      post :create, :session => @attr
+      response.should render_template('new')
+    end
+
+#    it "should have the right title" do
+#      post :create, :session => @attr
+#      response.should have_selector("title", :content => "Sign in")
+#    end
+
+#    it "should have a flash.now message" do
+#      post :create, :session => @attr
+#      flash.now[:error].should =~ /invalid/i
+#    end
+  end
+
 
 
 end
