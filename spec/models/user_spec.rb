1a4231e68a803e3d94b370c6eb16889c17ad357e
Carl Crott <carlcrott@gmail.com>
2013-03-13 11:26:31 -0700

    added initial model test for user fields

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe User do
+  before { @user = User.new(login: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:login) }
+  it { should respond_to(:email) }
+  it { should respond_to(:page_access_level) }
+  it { should respond_to(:access_level) }
+  
+end
