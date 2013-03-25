b42173fc7aa1afaf569f5eddbd2b67be1c3a8ed4
Rob Kooper <kooper@illinois.edu>
2013-03-25 15:45:29 -0500

    make user 1 the default

--- config/additional_environment_vm.rb
+++ config/additional_environment_vm.rb
@@ -7,3 +7,5 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default_charset = "utf-8"
 config.action_mailer.delivery_method = :smtp
+
+config.default_user=1
