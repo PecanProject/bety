442f61264bce9f2f22166a4534722e700c77b4f2
Carl Crott <carlcrott@gmail.com>
2013-04-04 11:53:35 -0500

    sites redesigned with tests

--- spec/features/site_integration_spec.rb
+++ spec/features/site_integration_spec.rb
@@ -7,9 +7,49 @@ feature 'Sites index works' do
   end
 
   context 'GET /sites' do
-    it 'should have "Listing sites" ' do
+    it 'should have "Listing Sites" ' do
       visit '/sites'
-      page.should have_content 'Listing sites'
+      page.should have_content 'Listing Sites'
+    end
+
+    it 'should allow creation of new sites' do
+      visit '/sites/new'
+      fill_in 'Site name', with:'tester'
+      fill_in 'Elevation (m)', with: '2900'
+      fill_in 'Mean Annual Precipitation (mm/yr)', with:'11/90'
+      fill_in 'Mean Annual Temperature (°C)', with: '90009'
+      fill_in 'City', with:'Taipei'
+      fill_in 'State', with:'hungsha'
+      select('UNITED STATES', :from => 'Country')
+      fill_in 'Notes', with: 'working with and manipulating those elements'
+      fill_in 'Soil Notes', with: 'methods available, so you can restrict them to specific parts of the page'
+      click_button 'Create'
+      
+      page.should have_content 'Site was successfully created'
+    end
+
+    context 'clicking view site button' do
+      it 'should return "Viewing Site" ' do
+        visit '/sites/'
+        first(:xpath,".//a[@alt='show' and contains(@href,'/sites/')]").click
+        page.should have_content 'Viewing Site'
+      end
+    end
+    
+    context 'clicking edit site button' do
+      it 'should return "Editing Site" ' do
+        visit '/sites/'
+        first(:xpath,".//a[@alt='edit' and contains(@href,'/edit')]").click
+        page.should have_content 'Editing Site'
+      end
+    end
+
+    context 'clicking use site button' do
+      it 'should return "Sites already associated with this site" ' do
+        visit '/sites/'
+        first(:xpath,".//a[@alt='use' and contains(@href,'/use_site/')]").click
+        page.should have_content 'Sites already associated with this site'
+      end
     end
 
   end
