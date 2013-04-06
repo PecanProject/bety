fadc23d4434081f65801838f53c0459b98777ddd
Carl Crott <carlcrott@gmail.com>
2013-04-06 18:14:06 -0500

    managements redesigned with tests

--- spec/features/management_integration_spec.rb
+++ spec/features/management_integration_spec.rb
@@ -7,9 +7,29 @@ feature 'Managements index works' do
   end
 
   context 'GET /managements' do
-    it 'should have "Listing managements" ' do
+    it 'should have "Listing Managements" ' do
       visit '/managements'
-      page.should have_content 'Listing managements'
+      page.should have_content 'Listing Managements'
+    end
+
+    it 'should allow creation of new managments' do
+      ## pending
+    end
+
+    context 'clicking view managment button' do
+      it 'should return "Viewing Management" ' do
+        visit '/managements/'
+        first(:xpath,".//a[@alt='show' and contains(@href,'/managements/')]").click
+        page.should have_content 'Viewing Management'
+      end
+    end
+    
+    context 'clicking edit managment button' do
+      it 'should return "Editing Management" ' do
+        visit '/managements/'
+        first(:xpath,".//a[@alt='edit' and contains(@href,'/edit')]").click
+        page.should have_content 'Editing Management'
+      end
     end
 
   end
