3bf1c9adb953e9ae845168a025039e2fa1062a14
Scott Rohde <srohde@igb.illinois.edu>
2013-06-19 16:23:23 -0500

    Always used exactly the view's column names in the CSV header.

--- app/models/traits_and_yields_view.rb
+++ app/models/traits_and_yields_view.rb
@@ -7,33 +7,33 @@ class TraitsAndYieldsView < ActiveRecord::Base
 
 
   comma do
-    result_type
-    id
-    citation_id
-    site_id
-    treatment_id
-    sitename
-    city
-    lat
-    lon
-    scientificname
-    commonname
-    genus
-    author
-    citation_year
-    treatment
-    date
-    month
-    year
-    dateloc
-    trait
-    mean
-    units
-    n
-    statname
-    stat
-    notes
-    user_name
+    result_type 'result_type'
+    id 'id'
+    citation_id 'citation_id'
+    site_id 'site_id'
+    treatment_id 'treatment_id'
+    sitename 'sitename'
+    city 'city'
+    lat 'lat'
+    lon 'lon'
+    scientificname 'scientificname'
+    commonname 'commonname'
+    genus 'genus'
+    author 'author'
+    citation_year 'citation_year'
+    treatment 'treatment'
+    date 'date'
+    month 'month'
+    year 'year'
+    dateloc 'dateloc'
+    trait 'trait'
+    mean 'mean'
+    units 'units'
+    n 'n'
+    statname 'statname'
+    stat 'stat'
+    notes 'notes'
+    user_name 'user_name'
   end
 
 end
