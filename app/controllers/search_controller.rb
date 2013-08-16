243b980c4c493ffb481c1ad8ccdb98e6df9335e4
Scott Rohde <srohde@igb.illinois.edu>
2013-08-16 14:58:15 -0500

    Added contact e-mail to header for CSV file, and ensure that commas and quotes within a field are properly escaped.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,19 +5,26 @@ class SearchController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   HEADER = <<CREDITS
-David LeBauer, Dan Wang, and Michael Dietze, 2010.\
-Biofuel Ecophysiological Traits and Yields Database Version 1.0.\
+David LeBauer, Dan Wang, and Michael Dietze, 2010.  \
+Biofuel Ecophysiological Traits and Yields Database Version 1.0.  \
 Energy Biosciences Institute, Urbana, IL
 CREDITS
+  HEADER.chomp!
+
+  CONTACT_EMAIL = "dlebauer@illinois.edu"
 
   FORMAT_STRING = <<FORMAT
-# %<credits>s#
-# SQL query: %<query>s
-#
-# Date of query: %<date>s
-#
+%<credits>s
+
+Contact:,%<contact_email>s
+
+SQL query:,%<query>s
+
+Date of query:,%<date>s
+
 %<data>s
 FORMAT
+
 
   def index
     #if params[:format].nil? or params[:format] == 'html'
@@ -36,12 +43,19 @@ FORMAT
       format.xml  { render :xml => @results }
       format.csv do 
         sql_query = TraitsAndYieldsView.search(params[:search]).to_sql
-        str = sprintf(FORMAT_STRING, credits: HEADER, query: sql_query, date: Time.now, data: @results.to_comma)
+        # escape any fields that may contain interal commas or double quotes:
+        str = sprintf(FORMAT_STRING, credits: escape_for_excel(HEADER), contact_email: CONTACT_EMAIL, query: escape_for_excel(sql_query), date: Time.now, data: @results.to_comma)
         send_data str, type: Mime::CSV,
         disposition: "attachment; filename=search_results.csv"
       end
       format.json  { render :json => @results }
     end
+  end
+
+  private
+  def escape_for_excel(str)
+    str.gsub '"', '""'
+    return "\"#{str}\""
   end
       
 end
