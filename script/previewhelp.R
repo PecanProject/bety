7da3838be82dcf23e1515d8972a7c3cb1523222e
Jeremy Kemball <jkemball@gmail.com>
2013-08-27 16:26:07 -0500

    Fixed weird image size issue

--- script/previewhelp.R
+++ script/previewhelp.R
@@ -10,7 +10,7 @@ n <- as.numeric( args[8] )
 
 prior.density <- pr.dens(priorname, aparam, bparam)
 
-png(imgfile, height = 80, width = 80)
+png(imgfile, height = 220, width = 220)
 p<- ggplot(data = prior.density, aes(x = x, y = y))
 p <- p + geom_line()
 p <- p + scale_y_continuous(breaks = NULL)
