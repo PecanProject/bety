e16cb474984ed1ed895727e86cfc04bee04d4369
Jeremy Kemball <jkemball@gmail.com>
2013-09-03 15:26:43 -0500

    final image size changes

--- script/previewhelp.R
+++ script/previewhelp.R
@@ -10,12 +10,12 @@ n <- as.numeric( args[8] )
 
 prior.density <- pr.dens(priorname, aparam, bparam)
 
-png(imgfile, height = 220, width = 220)
+png(imgfile, height = 100, width = 200)
 p<- ggplot(data = prior.density, aes(x = x, y = y))
 p <- p + geom_line()
 p <- p + scale_y_continuous(breaks = NULL)
 p <- p + xlab(NULL) + ylab(NULL)
-p <- p + ggtitle(paste(priorname, '(', aparam, ',', bparam, ')', 'N=', n))
+p <- p + ggtitle(paste(priorname, '(', aparam, ',', bparam, ')', ' N=', n))
 p + theme_minimal()
 
 dev.off()
