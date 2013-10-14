PecanProject/bety

commit 3fec0a6a5e1dadda0ccc22ac4ef2bba4df37a25f

Added code to ignore certain keystrokes in search box.

public/javascripts/lazy/simple_search.js
@@ -7,6 +7,8 @@ var delay = (function () {
 })();
 
 var search_function = function (event, locationParameters) {
+    if (is_ignored(event)) return;
+
     if (!locationParameters) {
         var locationParameters = {};
     }
@@ -21,6 +23,32 @@ var search_function = function (event, locationParameters) {
         return false;
     }, 1000);
 }
+
+// Returns true if "event" is a keyup event on a key we are not
+// interested in.
+function is_ignored(event) {
+    if (event.type == "keyup") {
+        var keyCode = event.which;
+
+        console.log(keyCode);
+
+        var dontIgnore = false;
+
+        // Don't ignore alphabetic keys:
+        if (65 <= keyCode && keyCode <= 90) {
+            dontIgnore = true;
+        }
+
+        // Don't ignore backspace
+        if (keyCode == 8) {
+            dontIgnore = true;
+        }
+        
+       return !dontIgnore;
+    }
+    return false;
+}
+        
 
 function remove_search_term_restriction() {
     jQuery('#simple_search input#search').val('');

commit dfb9828e63e6d0d8874733ae112352259340f994

Fixed bug introduced in commit 3fec0a6a5e1dadda0ccc22ac4ef2bba4df37a25f: we need to ensure 'event' is non-null before calling 'is_ignored' on it.
Removed debug code.

public/javascripts/lazy/simple_search.js
@@ -12,7 +12,7 @@ var delay = (function () {
 // and the 'Show/Hide Map' button, and by the 'search_by_location'
 // function.
 var search_function = function (event, locationParameters) {
-    if (is_ignored(event)) return;
+    if (event && is_ignored(event)) return;
 
     if (!locationParameters) {
         var locationParameters = {};
@@ -34,8 +34,6 @@ var search_function = function (event, locationParameters) {
 function is_ignored(event) {
     if (event.type == "keyup") {
         var keyCode = event.which;
-
-        console.log(keyCode);
 
         var dontIgnore = false;
 