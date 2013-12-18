c67001d0f8b19f7e9e6594e86c47219afa1a9376
Scott Rohde <srohde@igb.illinois.edu>
2013-12-18 11:16:54 -0600

    Draft of Ajax version of bulk upload.

--- app/controllers/bulk_upload_controller.rb
+++ app/controllers/bulk_upload_controller.rb
@@ -1,28 +1,72 @@
 class BulkUploadController < ApplicationController
+
   def start_upload
   end
 
-  def confirm_data
-
-    csv = CSV.open(params["CSV file"].path, { headers: true }) # CSV.new(data, { headers: true })
-    
-    csv.readline
-
-    session[:headers] = @headers = csv.headers
-
-    @data = csv
-  end
-
-  def map_data
+  def finish_upload
 
     # hard code trait columns for now
-
     @columns = Trait.columns
+    @displayed_columns = @columns.select { |col| !['id', 'created_at', 'updated_at'].include?(col.name) }
 
-    logger.info(@columns[0].inspect)
+    # get CSV file path
+    session[:csv_file_path] = @csv_file_path = params["CSV file"].path
 
-    @headers = session[:headers] || "???"
+    begin
+      csv = CSV.open(params["CSV file"].path, { headers: true }) # CSV.new(data, { headers: true })
+      csv.readline
+      @headers = session[:headers] = csv.headers
+      
+      data = csv.read # force exception if not well formed
+
+      rows_of_hashes = []
+
+      data.each do |row|
+        rows_of_hashes << row.to_hash
+      end
+
+      @data_json = ActiveSupport::JSON.encode(rows_of_hashes)
+      csv.rewind
+
+      @displayed_columns_json = ActiveSupport::JSON.encode(@displayed_columns.map { |col| col.name })
+
+      @data = csv
+    rescue CSV::MalformedCSVError => e
+      @errors = e
+    end
+
+    respond_to do |format|
+      format.html {
+        if @errors
+          render action: "start_upload"
+        else
+          render
+        end
+      }
+    end
+  end
+
+
+  # These are the stages handled by finish_upload:
+
+  # display_CSV_stage
+
+  # mapping_stage
+  def map_data
+    #@headers = session[:headers]
+
+    respond_to do |format|
+      format.js { render layout: false }
+    end
+  end
+
+  # validation_stage
+  def confirm_data
     
+
+    respond_to do |format|
+      format.js { render :layout => false }
+    end
   end
 
 
--- app/helpers/bulk_upload_helper.rb
+++ app/helpers/bulk_upload_helper.rb
@@ -1,5 +1,65 @@
 module BulkUploadHelper
 
+  def default_source_for(column)
+    @headers = session[:headers]
+
+    case column
+      when /(.+)_id/,  "date", "dateloc", "time", "timeloc", "access_level"
+      if @headers.include?(column)
+        use = "use the value of CSV column</td><td class='column_name'>#{column}"
+      else
+        if column == "specie_id" && 
+            (@headers.include?("scientificname") || @headers.include?("species.scientificname"))
+          use = "use the id for the row in the species table corresponding to the value of CSV column</td><td class='column_name'>" + (@headers.include?("species.scientificname") ? "species.scientificname" : "scientificname")
+        elsif column == "access_level"
+          use = "use user-supplied value</td><td>#{text_field_tag (column.to_sym), "4"}"
+        else
+          use = "use user-supplied value</td><td>#{text_field_tag (column.to_sym)}"
+        end
+      end
+
+      when /date_(.+)/
+      if @headers.include?(column)
+        use = "use the value of CSV column</td><td class='column_name'>#{column}"
+      elsif @headers.include?("date")
+        use = "use the value of SQL #{$1.upcase} function applied to CSV column</td><td class='column_name'>date"
+      else
+        use = "use user-supplied value</td><td>#{text_field_tag (column.to_sym)}"
+      end
+
+
+      when /time_(.+)/
+      if @headers.include?(column)
+        use = "use the value of CSV column</td><td class='column_name'>#{column}"
+      elsif @headers.include?("time")
+        use = "use the value of SQL #{$1.upcase} function applied to CSV column</td><td class='column_name'>time"
+      else
+        use = "use user-supplied value</td><td>#{text_field_tag (column.to_sym)}"
+      end
+
+      
+      when "checked"
+      use = "always use</td><td>0"
+
+      when "mean", "stat", "statname", "n"
+      if @headers.include?(column)
+        use = "use the value of CSV column</td><td class='column_name'>#{column}"
+      else
+        use = "database default</td><td>#{column.default || "NULL"}"
+      end
+
+      when "notes"
+      if @headers.include?(column)
+        use = "use the value of CSV column</td><td class='column_name'>#{column}"
+      else
+        use = "(leave blank)</td><td>"
+      end
+      
+      else
+      use = "DEFAULT"
+    end
+    use
+  end
 
   def source(dbcolumn)
     @headers = session[:headers]
