33652866b2b92307a3ef444c406f4d1a3c1a76da
Scott Rohde <srohde@igb.illinois.edu>
2014-03-03 16:16:06 -0600

    Changing to using jQuery UI's autocompletion library.

--- public/javascripts/lazy/bulk_upload.js
+++ public/javascripts/lazy/bulk_upload.js
@@ -1,27 +1,86 @@
 jQuery(function() {
 
-    var sites = new Bloodhound({
-        datumTokenizer: function(d) {  return Bloodhound.tokenizers.whitespace(d.name); },
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        limit: 10,
-        local: [
-            { name: 'test'}
-        ],
-        prefetch: {
-            url: 'http://localhost:3000/sites.json',
-            filter: function(list) {
-                return jQuery.map(jQuery.grep(list, function(item) { return (item["value"] != "" && item["value"] !== null); }), function(site) { return { name: site["value"] }; });
+
+    if (jQuery('#autocomplete_site').length) {
+        var sites;
+        jQuery.get("/sites/autocomplete.json", function( data ) {
+            sites = data;
+        });
+
+        jQuery('#autocomplete_site').autocomplete({
+            
+            source: function(request, response) {
+                var matcher = new RegExp("^" + jQuery.ui.autocomplete.escapeRegex( request.term ), "i" );
+                
+                response( jQuery.grep( sites, function( item ) {
+                    return matcher.test( item );
+                }) );
             }
-        }
-    });
+        });
+    }
+
+
+    if (jQuery('#autocomplete_species').length) {
+        /*
+        var species;
+        jQuery.get("/species/autocomplete.json", function( data ) {
+            species = data;
+        });
+        */
+        jQuery('#autocomplete_species').autocomplete({
+            /*
+            source: function(request, response) {
+                var matcher = new RegExp("^" + jQuery.ui.autocomplete.escapeRegex( request.term ), "i" );
+                
+                response( jQuery.grep( species, function( item ) {
+                    return matcher.test( item );
+                }) );
+            }
+            */
+            source: "/species/autocomplete.json"
+        });
+    }
+
+
+    if (jQuery('#autocomplete_treatment').length) {
+        var treatments;
+        jQuery.get("/treatments/autocomplete.json", function( data ) {
+            treatments = data;
+        });
+
+        jQuery('#autocomplete_treatment').autocomplete({
+            
+            source: function(request, response) {
+                var matcher = new RegExp("^" + jQuery.ui.autocomplete.escapeRegex( request.term ), "i" );
+                
+                response( jQuery.grep( treatments, function( item ) {
+                    return matcher.test( item );
+                }) );
+            }
+        });
+    }
+
+
+    if (jQuery('#autocomplete_cultivar').length) {
+        var cultivars;
+        jQuery.get("/cultivars/autocomplete.json", function( data ) {
+            cultivars = data;
+        });
+
+        jQuery('#autocomplete_cultivar').autocomplete({
+            
+            source: function(request, response) {
+                var matcher = new RegExp("^" + jQuery.ui.autocomplete.escapeRegex( request.term ), "i" );
+                
+                response( jQuery.grep( cultivars, function( item ) {
+                    return matcher.test( item );
+                }) );
+            }
+        });
+    }
+
+
+
     
-    sites.initialize();
-    
-    // instantiate the typeahead UI
-    jQuery('#autocomplete_site').typeahead(null, {
-        name: 'sites',
-        displayKey: 'name',
-        source: sites.ttAdapter()
-    });
 
 });
