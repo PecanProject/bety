d3c4f7c02b9dcbe5f69286e569f2ef37ed35bb34
Scott Rohde <srohde@igb.illinois.edu>
2014-03-04 17:09:27 -0600

    Fixed last commit: Using root_url in place of root_path works under WEBrick as well as apache.

--- public/javascripts/lazy/bulk_upload.js
+++ public/javascripts/lazy/bulk_upload.js
@@ -1,29 +1,31 @@
 jQuery(function() {
     // initialize any and all autocompletion fields found on the page:
 
-        jQuery('#autocomplete_site').autocomplete({
-            source: ROOT_PATH + "/sites/autocomplete.json"
-        });
+    // to-do: options should be restricted by citation choosen
+    jQuery('#autocomplete_site').autocomplete({
+        source: ROOT_URL + "/sites/autocomplete.json"
+    });
 
-        jQuery('#autocomplete_species').autocomplete({
-            source: ROOT_PATH + "/species/autocomplete.json",
+    jQuery('#autocomplete_species').autocomplete({
+        source: ROOT_URL + "/species/autocomplete.json",
 
-            // update source for the cultivars fields: cultivar suggestions depend on what species was chosen
-            change: function(event, ui) {
-                jQuery('#autocomplete_cultivar').autocomplete("option", "source", ROOT_PATH + "/cultivars/autocomplete.json?species=" + jQuery('#autocomplete_species').val())
-            }
-        });
+        // update source for the cultivars fields: cultivar suggestions depend on what species was chosen
+        change: function(event, ui) {
+            jQuery('#autocomplete_cultivar').autocomplete("option", "source", ROOT_URL + "/cultivars/autocomplete.json?species=" + jQuery('#autocomplete_species').val())
+        }
+    });
 
-        jQuery('#autocomplete_treatment').autocomplete({
-            source: ROOT_PATH + "/treatments/autocomplete.json"
-        });
+    // to-do: options should be restricted by citation choosen
+    jQuery('#autocomplete_treatment').autocomplete({
+        source: ROOT_URL + "/treatments/autocomplete.json"
+    });
 
-        jQuery('#autocomplete_cultivar').autocomplete({
-            // set source to this until it gets updated by the species field change method
-            source: function(request, response) {
-                // no matter what the user types, this option will be all that is displayed until a species is chosen
-                response([{ label: "Please choose a species", value: null}])
-            }
-        });
+    jQuery('#autocomplete_cultivar').autocomplete({
+        // set source to this until it gets updated by the species field change method
+        source: function(request, response) {
+            // no matter what the user types, this option will be all that is displayed until a species is chosen
+            response([{ label: "Please choose a species", value: null}])
+        }
+    });
 
 });
