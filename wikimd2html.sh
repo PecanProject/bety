6e4fa6d5de0c44854f26c732301bf6a818495cf1
David LeBauer <dlebauer@gmail.com>
2014-04-01 20:43:06 -0500

    moved 'readme' to 'index' exported using for file in *.md; do ~/.cabal/bin/pandoc ${file} -s --mathjax --from markdown_github --standalone --highlight-style pygments -o ${file}.html; done

--- wikimd2html.sh
+++ wikimd2html.sh
@@ -1,1 +1,3 @@
-for file in *.md; do sudo grip --gfm --context=PecanProject/bety --export  title.md; done
+for file in *.md; do ~/.cabal/bin/pandoc title.md -s --mathjax -o title.md.html; done
+rename 's/\.md//' *.md.html 
+git push --force origin gh-pages 
