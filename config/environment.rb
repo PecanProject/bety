9221fed73796bbda187fbe57077bbdd70b33ca65
Rob Kooper <kooper@illinois.edu>
2014-04-24 00:21:08 -0500

    make sure next table has id of bigint

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BetyRails3::Application.initialize!
+
+# config/initializers/bigint_primary_keys.rb
+ActiveRecord::Base.establish_connection
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:primary_key] = 'bigserial primary key'
+ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = "BIGINT UNSIGNED DEFAULT NULL auto_increment PRIMARY KEY"
