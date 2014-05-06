3a0138f595bdd9958ec69d0abe7f53922fca65df
Scott Rohde <srohde@igb.illinois.edu>
2014-05-06 13:02:00 -0500

    Check whether ConnectionAdapter constants are defined before trying to use them so that the environment works in both PostgreSQL and MySQL.

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,9 @@ BetyRails3::Application.initialize!
 
 # config/initializers/bigint_primary_keys.rb
 ActiveRecord::Base.establish_connection
-ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:primary_key] = 'bigserial primary key'
-ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = "BIGINT UNSIGNED DEFAULT NULL auto_increment PRIMARY KEY"
+if defined? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:primary_key] = 'bigserial primary key'
+end
+if defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = "BIGINT UNSIGNED DEFAULT NULL auto_increment PRIMARY KEY"
+end
