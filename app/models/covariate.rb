da6514412a43c85438a3c6399be6303839d778b9
Scott Rohde <srohde@igb.illinois.edu>
2014-05-15 14:07:31 -0500

    Added Rails-level validation of the level attribute to ensure it is in the allowable range for the given variable.

--- app/models/covariate.rb
+++ app/models/covariate.rb
@@ -6,6 +6,18 @@ class Covariate < ActiveRecord::Base
   SEARCH_INCLUDES = %w{ variable }
   SEARCH_FIELDS = %w{ variables.name covariates.level covariates.n covariates.stat covariates.statname }
 
+  validate :level_in_range
+
+  def level_in_range
+    v = Variable.find(variable_id)
+    if !v.min.nil? and level < v.min.to_f
+      errors.add(:level, "The value of level for the #{v.name} trait must be at least #{v.min}.")
+    end
+    if !v.max.nil? and level > v.max.to_f
+      errors.add(:level, "The value of level for the #{v.name} trait must be at most #{v.max}.")
+    end
+  end
+  
   scope :sorted_order, lambda { |order| order(order).includes(SEARCH_INCLUDES) }
   scope :search, lambda { |search| where(simple_search(search)) }
 
