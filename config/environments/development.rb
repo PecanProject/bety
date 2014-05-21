727d1cffb8e0de4e1c15394b4093cbca97351dfa
Rob Kooper <kooper@illinois.edu>
2014-05-21 10:58:51 -0700

    allow for running with rails server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 BetyRails3::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.active_record.schema_format = :sql
+
+  config.action_controller.relative_url_root = ""
+  ActionController::Base.config.relative_url_root = ''
+
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
