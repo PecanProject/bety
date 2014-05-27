5d791d90573c45794f402e4ae1edcf2ff18ff710
yln <sourgrapes612@hotmail.com>
2014-05-27 09:43:05 -0500

    .

--- spec/features/posterior_search_spec.rb
+++ spec/features/posterior_search_spec.rb
@@ -15,10 +15,9 @@ feature 'Posteriors search' do
 
 
   context 'search on posteriors' do
-    it 'should show search result on current page', :js => true do
+    it 'should show search result on current page' do
       visit '/posteriors'
       fill_in 'search', :with => 'testsearch'        
-      page.execute_script("e = jQuery.Event('keyup');e.keyCode= 73; jQuery('#simple_search').trigger(e);")
       expect(page).to have_content 'No entries'
     end
     it 'should show search result on new page' do
