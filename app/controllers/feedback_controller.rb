d473f76b78aeff39d79855d33980be569f8d7473
yln <sourgrapes612@hotmail.com>
2014-06-21 11:02:37 -0500

    .

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,6 +1,6 @@
 class FeedbackController < ApplicationController
 	def feedback_email
-		#ContactMailer::feedback_email(params[:feedback_email]).deliver
+		ContactMailer::feedback_email(params[:feedback_email]).deliver
     	#`echo "rails email test" | mail -s test_email sourgrapes612@hotmail.com`
 		respond_to do |format|
 			format.js {render :layout =>false}
