adc979f6d1488af6e66ac8953957cfd1c3c975f3
phenolphtalein <phenolphtalein@users.noreply.github.com>
2014-06-23 11:38:58 -0500

    Update feedback_controller.rb

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,9 +1,8 @@
 class FeedbackController < ApplicationController
-	def feedback_email
-		ContactMailer::feedback_email(params[:feedback_email]).deliver
-    	#`echo "rails email test" | mail -s test_email sourgrapes612@hotmail.com`
-		respond_to do |format|
-			format.js {render :layout =>false}
-		end
-	end
+  def feedback_email
+    ContactMailer::feedback_email(params[:feedback_email]).deliver
+      respond_to do |format|
+        format.js {render :layout =>false}
+      end
+    end
 end
