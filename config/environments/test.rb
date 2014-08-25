f34f6ad5aaf8a2fb27f705e057b4d3936c4687c6
Rob Kooper <kooper@illinois.edu>
2014-08-25 13:33:32 -0500

    removed warning message
    ActionController::Base.relative_url_root= is ineffective. Please stop
    using it.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,7 +2,6 @@ BetyRails3::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # These lines are a temporary hack to allow running Rails 3.0.18 under Ruby 2.0:
-  config.action_controller.relative_url_root = ""
   ActionController::Base.config.relative_url_root = ''
 
   # The test environment is used exclusively to run your application's
