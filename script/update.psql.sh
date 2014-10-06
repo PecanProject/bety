fa79780f3428874734933de2aef9097fbbaa3912
Scott Rohde <srohde@igb.illinois.edu>
2014-10-06 12:17:53 -0500

    Incorporated some features from pecan's load.bety.sh script, in particular, adding default users with various permission levels and the guestuser.

--- script/update.psql.sh
+++ script/update.psql.sh
@@ -2,17 +2,93 @@
 
 set -x
 
+# ----------------------------------------------------------------------
+# START CONFIGURATION SECTION
+# ----------------------------------------------------------------------
+
+# name of the dabase to load
+# this script assumes the user running it has access to the database
+DATABASE=${DATABASE:-"bety"}
+
+# owner of the database
+# also used to connect to the database for most operations
+OWNER=${OWNER:-"bety"}
+
+# psql options
+# this allows you to add any other options
+PG_OPT=${PG_OPT:-""}
+
+# Keep the dump file even if the update failed?
+# Set this to YES to keep the update file; this is helpful for debugging the
+# script. The default value is NO and the update file will be removed.
+# TODO: Make this work!
+KEEPTMP=${KEEPTMP:-"NO"}
+
+# If this is set to YES, one user will be converted to carya with password. This
+# will give this user admin priviliges. It will also create 16 more users that
+# have specific abilities and create the guest user.  Set USERS to NO to skip
+# these steps.
+USERS=${USERS:-YES}
+ 
+# ----------------------------------------------------------------------
+# END CONFIGURATION SECTION
+# ----------------------------------------------------------------------
+
+
 # command to connect to database
 if [ "`uname -s`" != "Darwin" ]; then
   export POSTGRES="sudo -u postgres"
 fi
-export CMD="${POSTGRES} psql -U bety"
+export CMD="${POSTGRES} psql -U ${OWNER}"
 
 # load latest dump of the database
 curl -o betydump.gz https://ebi-forecast.igb.illinois.edu/pecan/dump/betydump.psql.gz
 
-${POSTGRES} dropdb bety
-${POSTGRES} createdb -O bety bety
+${POSTGRES} dropdb ${DATABASE}
+${POSTGRES} createdb -O ${OWNER} ${DATABASE}
 
-gunzip -c betydump.gz | ${CMD} bety
+gunzip -c betydump.gz | ${CMD} ${DATABASE}
+
+if [ "${USERS:-YES}" == "YES" ]; then
+  ID=2
+
+  RESULT=$( psql ${PG_OPT} -U ${OWNER} -t -d "${DATABASE}" -c "SELECT count(id) FROM users WHERE login='carya';" )
+  if [ ${RESULT} -eq 0 ]; then
+    RESULT='UPDATE 0'
+    while [ "${RESULT}" = "UPDATE 0" ]; do
+      RESULT=$( psql ${PG_OPT} -U ${OWNER} -t -d "${DATABASE}" -c "UPDATE users SET login='carya', name='carya', crypted_password='df8428063fb28d75841d719e3447c3f416860bb7', salt='carya', access_level=1, page_access_level=1 WHERE id=${ID};" )
+      ((ID++))
+    done
+  fi
+  echo "Added carya with admin privileges"
+
+  # set all users
+  for f in 1 2 3 4; do
+    for g in 1 2 3 4; do
+      RESULT=$( psql ${PG_OPT} -U ${OWNER} -t -d "${DATABASE}" -c "SELECT count(id) FROM users WHERE login='carya${f}${g}';" )
+      if [ ${RESULT} -eq 0 ]; then
+        RESULT='UPDATE 0'
+        while [ "${RESULT}" = "UPDATE 0" ]; do
+          RESULT=$( psql ${PG_OPT} -U ${OWNER} -t -d "${DATABASE}" -c "UPDATE users SET login='carya${f}${g}', name='carya a-${f} p-${g}', crypted_password='df8428063fb28d75841d719e3447c3f416860bb7', salt='carya', access_level=${f}, page_access_level=${g} WHERE id=${ID};" )
+          ((ID++))
+        done
+      fi
+    done
+  done
+  echo "Updated users to have login='caryaXY' with appropriate privileges"
+  echo "  (X=access_level, Y=page_access_level)."
+
+  # add guest user
+  RESULT=$( psql ${PG_OPT} -U ${OWNER} -t -d "${DATABASE}" -c "SELECT count(id) FROM users WHERE login='guestuser';" )
+  if [ ${RESULT} -eq 0 ]; then
+    RESULT='UPDATE 0'
+    while [ "${RESULT}" = "UPDATE 0" ]; do
+      RESULT=$( psql ${PG_OPT} -U ${OWNER} -t -d "${DATABASE}" -c "UPDATE users SET login='guestuser', name='guestuser', crypted_password='994363a949b6486fc7ea54bf40335127f5413318', salt='bety', access_level=4, page_access_level=4 WHERE id=${ID};" )
+      ((ID++))
+    done
+  fi
+  echo "Added guestuser with access_level=4 and page_access_level=4"
+fi
+
+
 rm betydump.gz
