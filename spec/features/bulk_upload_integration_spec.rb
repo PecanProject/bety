d7e167bfa5eb9f5288c58d2647960042d6f749e9
Scott Rohde <srohde@igb.illinois.edu>
2014-10-20 14:34:30 -0500

    Added new tests for fuzzy matching.

--- spec/features/bulk_upload_integration_spec.rb
+++ spec/features/bulk_upload_integration_spec.rb
@@ -13,7 +13,7 @@ feature 'CSV file upload works' do
 
       visit '/bulk_upload/start_upload'
       attach_file 'CSV file', Rails.root.join('spec',
-                                              'fixtures', 
+                                              'fixtures',
                                               'files',
                                               'bulk_upload',
                                               'sample_yields.csv')
@@ -25,7 +25,7 @@ feature 'CSV file upload works' do
     it 'should not give an error when the citation chosen interactively but treatment is not' do
       visit '/bulk_upload/start_upload'
       attach_file 'CSV file', Rails.root.join('spec',
-                                              'fixtures', 
+                                              'fixtures',
                                               'files',
                                               'bulk_upload',
                                               'sample_yields_with_treatment_but_no_citation.csv')
@@ -40,7 +40,33 @@ feature 'CSV file upload works' do
       page.should have_content 'Please Verify Data-Set References Before Uploading'
       click_button 'Insert Data'
       page.should_not have_selector('.alert-error')
-    end      
+    end
+
+    it 'should successfully validate a file even when headings are not in canonical form' do
+      visit '/bulk_upload/start_upload'
+      attach_file 'CSV file', Rails.root.join('spec',
+                                              'fixtures',
+                                              'files',
+                                              'bulk_upload',
+                                              'data_validation',
+                                              'fuzzily_matching_headings.csv')
+      click_button 'Upload'
+      page.should_not have_selector('.alert-error')
+    end
+
+    it 'should successfully insert data from a file even when headings are not in canonical form' do
+      visit '/bulk_upload/start_upload'
+      attach_file 'CSV file', Rails.root.join('spec',
+                                              'fixtures',
+                                              'files',
+                                              'bulk_upload',
+                                              'data_validation',
+                                              'fuzzily_matching_headings.csv')
+      click_button 'Upload'
+      click_link 'Specify'
+      click_button 'Confirm'
+      click_button 'Insert Data'
+    end
 
 
   end
