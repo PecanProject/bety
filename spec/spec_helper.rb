f409dee27d218d2dac44c4db9c64380eab80f8dc
Scott Rohde <srohde@igb.illinois.edu>
2014-12-02 12:38:37 -0600

    Adding a dummy "Binding" class when javascript_driver is set to :webkit so we don't have to remove "binding.pry" breakpoints every time we switch back to the webkit driver.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,13 @@ RSpec.configure do |config|
   else
     Bundler.require('javascript_testing')
     Capybara.javascript_driver = :webkit
+
+
+    class Binding
+      def pry
+        puts "!!! binding.pry is for use with the Selenium JavaScript driver."
+      end
+    end
   end
 
 end
