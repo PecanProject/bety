298441e0c99d58f1b9bf1227e3086ee2c0159f64
Rob Kooper <kooper@illinois.edu>
2015-02-05 10:40:40 -0600

    use bigserial

--- db/migrate/20150202215147_add_id_many_to_many.rb
+++ db/migrate/20150202215147_add_id_many_to_many.rb
@@ -1,14 +1,14 @@
 class AddIdManyToMany < ActiveRecord::Migration
   def self.up
     execute %{
-      ALTER TABLE "citations_sites" ADD "id" serial;
-      ALTER TABLE "citations_treatments" ADD "id" serial;
-      ALTER TABLE "inputs_runs" ADD "id" serial;
-      ALTER TABLE "inputs_variables" ADD "id" serial;
-      ALTER TABLE "managements_treatments" ADD "id" serial;
-      ALTER TABLE "pfts_priors" ADD "id" serial;
-      ALTER TABLE "pfts_species" ADD "id" serial;
-      ALTER TABLE "posteriors_ensembles" ADD "id" serial;
+      ALTER TABLE "citations_sites" ADD "id" bigserial;
+      ALTER TABLE "citations_treatments" ADD "id" bigserial;
+      ALTER TABLE "inputs_runs" ADD "id" bigserial;
+      ALTER TABLE "inputs_variables" ADD "id" bigserial;
+      ALTER TABLE "managements_treatments" ADD "id" bigserial;
+      ALTER TABLE "pfts_priors" ADD "id" bigserial;
+      ALTER TABLE "pfts_species" ADD "id" bigserial;
+      ALTER TABLE "posteriors_ensembles" ADD "id" bigserial;
     }
   end
 
