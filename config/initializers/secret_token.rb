aba555d4f43e31f000dedc1f5f4d4aeafa4ddd61
Scott Rohde <srohde@igb.illinois.edu>
2015-03-27 13:46:30 -0500

    Moved production's secret token to an environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BetyRails3::Application.config.secret_token = '65f5a5b0f70fd46e2b2f697deb72648f619b4e1a313097b80fd70d678db0148654a0f9ce20e56515ef7bc4cdfb0cdcdb5ac0a54a8c06e9a791eed9eec14a1060'
+BetyRails3::Application.config.secret_token =  if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
+
