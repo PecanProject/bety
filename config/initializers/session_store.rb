e00a524d6665a94f88df2b88a9935a3a84a8e04a
Scott Rohde <srohde@igb.illinois.edu>
2015-04-07 15:06:26 -0500

    Switched to using the database for storing sessions.  Besides being in general a good idea, this overcomes some problems with the bulk upload feature when too many diverse records are being uploaded.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-BetyRails3::Application.config.session_store :cookie_store, :key => '_bety-rails3_session'
+#BetyRails3::Application.config.session_store :cookie_store, :key => '_bety-rails3_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# BetyRails3::Application.config.session_store :active_record_store
+BetyRails3::Application.config.session_store :active_record_store
