b7735870f00685e91ecf378632d7e85dee844f37
Scott Rohde <srohde@igb.illinois.edu>
2015-04-22 11:14:56 -0500

    Get rid of secret_tokey initializer now that we are using active record for storing sessions.
    Incorporate base name of Rails root into session key name so that different
    deployments with the same host domain will use different session keys.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-BetyRails3::Application.config.session_store :active_record_store
+BetyRails3::Application.config.session_store :active_record_store, key: "_#{Rails.root.basename.to_s}_session_id"
