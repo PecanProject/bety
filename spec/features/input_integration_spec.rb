eab99b552a44051977ba128ba6b512798abc0ed4
Scott Rohde <srohde@igb.illinois.edu>
2015-05-07 10:41:21 -0500

    Changed fixtures and RSpec tests so that running tests leaves the test database in the same state as when the tests were started except for sequence numbers and the sessions table.

--- spec/features/input_integration_spec.rb
+++ spec/features/input_integration_spec.rb
@@ -46,21 +46,8 @@ feature 'Inputs features work' do
       page.should_not have_link('X')
     end
 
-    it 'should allow linking and unlinking a file', js: true do
+    it 'should allow unlinking and re-linking a file', js: true do
       click_link 'View Related Files'
-
-      # add file
-      fill_in 'filesearch', with: 'CA'
-      sleep 1 # need time for search results to appear
-      first(:xpath, ".//a[text() = '+']").click
-
-      # If we're using Selenium, we have to deal with the modal dialogue:
-      if page.driver.is_a? Capybara::Selenium::Driver
-        a = page.driver.browser.switch_to.alert
-        a.accept
-      end
-
-      page.should have_link('X')
 
       # unlink file
       first(:xpath, ".//a[text() = 'X']").click
@@ -72,6 +59,19 @@ feature 'Inputs features work' do
       end
 
       page.should_not have_link('X')
+
+      # re-add file
+      fill_in 'filesearch', with: 'CA'
+      sleep 1 # need time for search results to appear
+      first(:xpath, ".//a[text() = '+']").click
+
+      # If we're using Selenium, we have to deal with the modal dialogue:
+      if page.driver.is_a? Capybara::Selenium::Driver
+        a = page.driver.browser.switch_to.alert
+        a.accept
+      end
+
+      page.should have_link('X')
     end
 
   end
