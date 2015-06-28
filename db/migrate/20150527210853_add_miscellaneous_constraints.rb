62c0200c1076f548141803760c3b1f2c4d166859
Scott Rohde <srohde@igb.illinois.edu>
2015-06-28 17:33:05 -0500

    Added projects.outdir check constraint per GH issue #244.

--- db/migrate/20150527210853_add_miscellaneous_constraints.rb
+++ db/migrate/20150527210853_add_miscellaneous_constraints.rb
@@ -4,12 +4,14 @@ class AddMiscellaneousConstraints < ActiveRecord::Migration
     # Use "%q" so that backspashes are taken literally (except when doubled).
     execute %q{
 ALTER TABLE "treatments" ADD CONSTRAINT "fk_treatments_users_1" FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE projects ADD CONSTRAINT outdir_value_sanity_check CHECK (outdir ~ '^/');
     }
   end
 
   def down
     execute %q{
 ALTER TABLE "treatments" DROP CONSTRAINT "fk_treatments_users_1";
+ALTER TABLE projects DROP CONSTRAINT outdir_value_sanity_check;
     }
   end
 end
