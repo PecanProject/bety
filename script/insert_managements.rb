f7f0caf12249e4566c90d057d7c419df891c63e8
Scott Rohde <srohde@igb.illinois.edu>
2015-06-29 13:07:55 -0500

    Added to help message.
    Moved constant declarations to top of file.

--- script/insert_managements.rb
+++ script/insert_managements.rb
@@ -1,4 +1,22 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+# define constants
+REQUIRED_HEADINGS = ['citation_author', 'citation_title', 'citation_year', 'treatment_name', 'mgmttype']
+OPTIONAL_HEADINGS = [ 'date', 'dateloc', 'level', 'units', 'notes' ]
+RECOGNIZED_HEADINGS = REQUIRED_HEADINGS + OPTIONAL_HEADINGS
+
+
+MANAGEMENT_INSERT_TEMPLATE = <<SQL
+INSERT INTO managements (citation_id, date, dateloc, mgmttype, level, units, notes, user_id)
+                 VALUES (     %i,      %s,    %s,      %s,       %s,    %s,    %s,     %i  );
+SQL
+
+MANAGEMENTS_TREATMENTS_INSERT_TEMPLATE = <<SQL
+INSERT INTO managements_treatments (treatment_id, management_id)
+                            VALUES (      %i,       LASTVAL()  );
+SQL
+
 
 
 def get_citation_from_row(row_as_hash)
@@ -42,16 +60,6 @@ def get_treatment_for_citation_and_row(citation, row_as_hash)
   return t
 
 end
-
-MANAGEMENT_INSERT_TEMPLATE = <<SQL
-INSERT INTO managements (citation_id, date, dateloc, mgmttype, level, units, notes, user_id)
-                 VALUES (     %i,      %s,    %s,      %s,       %s,    %s,    %s,     %i  );
-SQL
-
-MANAGEMENTS_TREATMENTS_INSERT_TEMPLATE = <<SQL
-INSERT INTO managements_treatments (treatment_id, management_id)
-                            VALUES (      %i,       LASTVAL()  );
-SQL
 
 def csvrow_to_input_statements(row_as_hash)
 
@@ -98,6 +106,123 @@ EOS
   opt :login, "The Rails login for the user running the script (required)", type: String, short: "-u"
   opt :output, "Output file", type: String, default: "new_managements.sql", short: "-o"
   opt :environment, "Rails environment to run in", type: String, default: "development", short: "-e"
+
+  banner <<-EOS
+
+DESCRIPTION
+
+This script takes a CSV file describing managments to be added to the database
+as input and outputs a file containing SQL statements to do the required insert
+statements.
+
+CSV FILE FORMAT
+
+The CSV file must contain the following column headings:
+\t#{REQUIRED_HEADINGS.join("\n\t")}
+
+Each row must have non-empty values in each of these columns.  Moreover, the
+citation columns must match exactly one row in the citations row of the database
+and the treatment name must match exactly one of the rows connected with the
+matched citation.
+
+Additionally, the CSV file MAY contain the following column headings:
+\t#{OPTIONAL_HEADINGS.join("\n\t")}
+
+Each optional column heading corresponds to a column in the database managements
+table.  Values in these columns may be left blank in any given row.  For these
+columns, if a column value is left blank, or if the column is not included in
+the table, the resulting column value in the database will be the SQL-defined
+default value for that column.
+
+DATABASE SPECIFICATION
+
+The database used by the script is determined by the environment specified by
+the "--environment" option (or "development" if not specified) and the contents
+of the configuration file config/database.yml.  (Run "rake dbconf" to view the
+contents of this file on the command line.)
+
+USING THE SCRIPT TO UPDATE THE PRODUCTION DATABASE
+
+There are essentially three options for using this script to update the
+production database.
+
+Option A: Run the script on the production server in the Rails root directory of
+the production deployment of the BETYdb Rails app.
+
+In detail:
+
+1. Upload the input CSV file to the production machine.
+
+2. Log in to the production machine and cd to the root directory of production
+   deployment of the BETYdb Rails app.
+
+3. Run the script using the "--environment=production" option and with "--login"
+   set to your own BETYdb Rails login for the production deployment.  The
+   command-line argument specifying the input CSV file path should match the
+   location you uploaded it to.
+
+4. After examining the resulting output file, apply it to the database with the
+   command
+
+       psql <production database name>  <  <output file name>
+
+Option B: Run the script on your local machine using an up-to-date copy of the
+BETYdb database.
+
+To do this:
+
+1. Switch to the root of the copy of the BETYdb Rails app you want to use.
+
+2. For the copy of the BETYdb database connected to this copy of the Rails app,
+   ensure that at least the citations and the treatments tables are up-to-date
+   with the production copy of the BETYdb database.  (If you have different
+   databases specified for your development and your production environments, be
+   sure that the enviroment you specify in the script options.)
+
+3. Run this script.
+
+4. Upload the output file to the production server and apply it to the
+   production database using the psql command given above.
+
+Option C: Run the script on your local machine using a Rails environment
+connected to the production database.
+
+1. Go to the copy of the BETYdb Rail app on your local machine that you wish to
+   use.
+
+2. Edit the file config/database.yml, adding the following section:
+
+ebi:
+  adapter: postgis
+  encoding: utf8
+  reconnect: false
+  database: <production database name>
+  pool: 5
+  username: <user name for connecting to the production database>
+  password: <password for the user specified above>
+  port: 8000
+  host: localhost
+
+Most of these values can be copied from the production copy config/database.yml
+if you have access to it.  The port and host entries are "new".
+
+3. Set up an ssh tunnel to the production server using the command
+
+ssh -L 8000:<production server address>:5432 <production server address>
+
+This will log you into the production server, but at the same time it will
+connect port 8000 on your local machine with port 5432 (the PostgreSQL server
+port) on the production machine.  (The choice of 8000 for port number is
+somewhat arbitrary, but whatever value you use should match the value you
+specified for the port number in the database.yml file.)
+
+4. Run this script with the environment option "--environment=ebi".  (Again, the
+name "ebi" for the environment is somewhat arbitrary, the the option value
+should match the name in your database.yml file.)
+
+5. Continue as in step 4 under option B.
+ 
+EOS
 end
 
 Trollop::die :login, "must be specified" if opts[:login].nil?
@@ -133,10 +258,6 @@ csv = CSV.open(csvpath, { headers: true })
 csv.readline # need to read first line to get headers
 headings = csv.headers
 
-REQUIRED_HEADINGS = ['citation_author', 'citation_title', 'citation_year', 'treatment_name', 'mgmttype']
-OPTIONAL_HEADINGS = [ 'date', 'dateloc', 'level', 'units', 'notes' ]
-RECOGNIZED_HEADINGS = REQUIRED_HEADINGS + OPTIONAL_HEADINGS
-
 if !(REQUIRED_HEADINGS - headings).empty?
   puts "ERROR: Your CSV file must contain the following columns (in any order): 'citation_author', 'citation_title', 'citation_year', 'treatment_name', 'mgmttype', 'level', 'units', 'date'"
   exit 1
@@ -164,7 +285,3 @@ csv.each do |row|
   end
 
 end
-
-
-
-
