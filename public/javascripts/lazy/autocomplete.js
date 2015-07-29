e41f59f4755640ea667cd84b4b3924fa9535148e
Scott Rohde <srohde@igb.illinois.edu>
2015-07-29 09:52:38 -0500

    Refactored code to make adding several autocompletion fields easier.

--- public/javascripts/lazy/autocomplete.js
+++ public/javascripts/lazy/autocomplete.js
@@ -1,26 +1,93 @@
-jQuery(function() {
-    // initialize any and all autocompletion fields found on the page:
 
-    jQuery('#search_sites').autocomplete({
-        source: ROOT_URL + "/sites/autocomplete.json"/*,
-        select: function(event, ui) {
-            event.preventDefault();
-            $("#search_sites").val(ui.item.label);
-            PK.render(ui.item.value);
-        }*/
-    });
+var completion_fields = {
+    "search_sites": { controller: "sites",
+                      hidden_field_id: "input_site_id" },
+    "search_inputs": { controller: "inputs",
+                       hidden_field_id: "input_parent_id" }
+};
 
-    jQuery('#search_sites').on("autocompleteselect", function(e, ui) {
+// We use this creation function to create the select function so that
+// the value of "autocompletion_widget_id" used in the function is the
+// value at the time the function was defined rather than the value at
+// the time it is run.  That is, we want a closure.
+function create_select_function(autocompletion_widget_id) {
+    return function(event, ui) {
+        // Prevent the value (the id number) of the item selected from
+        // displaying after a selection has been made ...
+        event.preventDefault();
 
-        // Prevent the value of the item selected from displaying after a
-        // selection has been made ...
-        e.preventDefault();
-
-        // ... and display the *value* of the item selected instead ...
-        this.value = ui.item.label;
+        // ... and display the *label* of the item selected instead ...
+        jQuery(this).val(ui.item.label);
 
         // ... but store the value of the item selected in a hidden field.
-        jQuery('#input_site_id').val(ui.item.value);
-    });
-    
+        jQuery('#' + completion_fields[autocompletion_widget_id].hidden_field_id).val(ui.item.value);
+    }
+}
+
+jQuery(function() {
+    // initialize any and all autocompletion fields found on the page:
+    for (autocompletion_widget_id in completion_fields) {
+        
+        jQuery('#' + autocompletion_widget_id).autocomplete({
+            source: ROOT_URL + "/" + completion_fields[autocompletion_widget_id].controller + "/autocomplete.json",
+            select: create_select_function(autocompletion_widget_id),
+            focus: function(event, ui) {
+                // Prevent the value (the id number) of an item selected from
+                // displaying when using the arrow keys ...
+                event.preventDefault();
+                
+                // ... and display the *label* of the item focused on instead.
+                jQuery(this).val(ui.item.label);
+            }
+        });
+    }
+
+
+    /*
+      jQuery('#search_sites').autocomplete({
+      source: ROOT_URL + "/sites/autocomplete.json",
+      select: function(event, ui) {
+      // Prevent the value (the id number) of the item selected from
+      // displaying after a selection has been made ...
+      event.preventDefault();
+
+      // ... and display the *label* of the item selected instead ...
+      jQuery(this).val(ui.item.label);
+
+      // ... but store the value of the item selected in a hidden field.
+      jQuery('#input_site_id').val(ui.item.value);
+      },
+      focus: function(event, ui) {
+      // Prevent the value (the id number) of an item selected from
+      // displaying when using the arrow keys ...
+      event.preventDefault();
+      
+      // ... and display the *label* of the item focused on instead.
+      jQuery(this).val(ui.item.label);
+      }
+      });
+
+      jQuery('#search_inputs').autocomplete({
+      source: ROOT_URL + "/inputs/autocomplete.json",
+      select: function(event, ui) {
+      // Prevent the value (the id number) of the item selected from
+      // displaying after a selection has been made ...
+      event.preventDefault();
+
+      // ... and display the *label* of the item selected instead ...
+      jQuery(this).val(ui.item.label);
+
+      // ... but store the value of the item selected in a hidden field.
+      jQuery('#input_parent_id').val(ui.item.value);
+      },
+      focus: function(event, ui) {
+      // Prevent the value (the id number) of an item selected from
+      // displaying when using the arrow keys ...
+      event.preventDefault();
+      
+      // ... and display the *label* of the item focused on instead.
+      jQuery(this).val(ui.item.label);
+      }
+      });
+    */
 });
