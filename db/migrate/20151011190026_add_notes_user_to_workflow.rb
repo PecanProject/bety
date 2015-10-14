136905231fae57e5ddd2c1062a85d1185a32f28d
Scott Rohde <srohde@igb.illinois.edu>
2015-10-14 15:12:39 -0500

    Added foreign-key constraint for new user_id column.

--- db/migrate/20151011190026_add_notes_user_to_workflow.rb
+++ db/migrate/20151011190026_add_notes_user_to_workflow.rb
@@ -2,6 +2,10 @@ class AddNotesUserToWorkflow < ActiveRecord::Migration
   def up
     add_column :workflows, :notes, :text
     add_column :workflows, :user_id, :integer, :limit => 8
+
+	execute %{
+ALTER TABLE "workflows" ADD CONSTRAINT "fk_workflows_users_1" FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+}
   end
 
   def down
