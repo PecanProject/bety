029d5ebe9ee5ec0083382c74d7cfd2907d50fcbf
Scott Rohde <srohde@igb.illinois.edu>
2015-11-06 17:01:35 -0600

    Refactored code: Moved methods from Trait and TraitsAndYieldsView classes into a shared module in an intializer.

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -493,33 +493,6 @@ class Trait < ActiveRecord::Base
     end
   end
 
-  def date_format
-    case dateloc
-    when 9
-      :no_date_data
-    when 8
-      :year_only
-    when 7
-      :season_and_year
-    when 6
-      :month_and_year
-    when 5.5
-      :week_of_year
-    when 5
-      :year_month_day
-    when 97
-      :season_only
-    when 96
-      :month_only
-    when 95
-      :month_day
-    when nil
-      :unspecified_dateloc
-    else
-      :unrecognized_dateloc
-    end
-  end
-
   def time_format
     case timeloc
     when 9
@@ -547,26 +520,7 @@ class Trait < ActiveRecord::Base
     return utctime.to_datetime
   end
 
-  # Convert the Trait date attribute (which is an ActiveSupport::TimeWithZone
-  # object) to a new TimeWithZone object representing the time in site_timezone.
-  # This is used in various methods used for presenting the date and time to the
-  # user, which is always done in local (site) time.
-  def date_in_site_timezone
-    date.in_time_zone(site_timezone)
-  end
-
-  # Returns the time zone of the associated site or "UTC" if no there is no
-  # associated site or if its time_zone attribute is blank.
-  def site_timezone
-    begin
-      zone = site.time_zone
-      if zone.blank?
-        zone = 'UTC'
-      end
-    rescue
-      zone = 'UTC' # site not found
-    end
-    return zone
-  end
+  # provides date_in_site_timezone, date_format, and site_timezone
+  include DateTimeUtilityMethods
 
 end
--- app/models/traits_and_yields_view.rb
+++ app/models/traits_and_yields_view.rb
@@ -118,55 +118,7 @@ class TraitsAndYieldsView < ActiveRecord::Base
     end
   end
 
-  private
-
-  # Convert the Trait date attribute (which is an ActiveSupport::TimeWithZone
-  # object) to a new TimeWithZone object representing the time in site_timezone.
-  # This is used in various methods used for presenting the date and time to the
-  # user, which is always done in local (site) time.
-  def date_in_site_timezone
-    date.in_time_zone(site_timezone)
-  end
-
-  # Returns the time zone of the associated site or "UTC" if no there is no
-  # associated site or if its time_zone attribute is blank.
-  def site_timezone
-    begin
-      zone = (Site.find(site_id)).time_zone
-      if zone.blank?
-        zone = 'UTC'
-      end
-    rescue
-      zone = 'UTC' # site not found
-    end
-    return zone
-  end
-
-  def date_format
-    case dateloc
-    when 9
-      :no_date_data
-    when 8
-      :year_only
-    when 7
-      :season_and_year
-    when 6
-      :month_and_year
-    when 5.5
-      :week_of_year
-    when 5
-      :year_month_day
-    when 97
-      :season_only
-    when 96
-      :month_only
-    when 95
-      :month_day
-    when nil
-      :unspecified_dateloc
-    else
-      :unrecognized_dateloc
-    end
-  end
+  # provides date_in_site_timezone, date_format, and site_timezone
+  include DateTimeUtilityMethods
 
 end
