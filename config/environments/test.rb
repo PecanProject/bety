9ba19716672439ef42d06143c5c61b2f34be574a
Scott Rohde <srohde@igb.illinois.edu>
2016-02-29 12:11:51 -0600

    Set config.assets.debug to true to get JavaScript-based tests to pass. Removed some redundant settings.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,11 +34,11 @@ BetyRails3::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
- 
-  # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=3600"
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
   config.assets.allow_debugging = true
+
+  # Expands the lines which load the assets.  This seems to be needed.  With the
+  # default setting (false), over 2/3 of the JavaScript-based tests fail.
+  config.assets.debug = true
 end
