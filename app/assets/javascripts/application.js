15d306adaeed1fec0ae764cc8cc7c010cda07d6c
Scott Rohde <srohde@igb.illinois.edu>
2016-02-29 12:29:57 -0600

    Removed most javascript_include_tag and stylesheet_link_tag calls from the application layout file by using asset manifest files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,46 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+// require jquery
+// require jquery_ujs
+// require jquery-ui
+
+// The above three lines might be able to replace the following 5.
+// TO-DO: Replace use jquery-rails and jquery-ui-rails gems in place of the
+// files in the lazy/ and libs/ directories.
+
+//= require lazy/jquery-1.11.0
+//= require libs/jquery-ui-1.10.4.custom/js/jquery-ui-1.10.4.custom.js
+//= require libs/jquery-migrate-1.2.1
+
+//= require lazy/jquery-1.7.2
+//= require lazy/jquery-ui-1.10.4.min
+
+
+// These lines replace "javascript_include_tag :all".  Note that we can't just
+// do "require_directory ." because we have to load prototype and effects first.
+//= require prototype
+//= require effects
+//= require bootstrap
+//= require controls
+//= require dragdrop
+//= require feedback-bundle
+//= require min
+//= require plugins
+//= require rails
+//= require script
+//= require window
+
+
+//= require libs/respond.min
+//= require libs/modernizr-2.0.6.min
+
+//= require lazy/simple_search
+//= require lazy/application
+
+// if sessions controller:
+//= require lazy/login
+
+// if maps or search controller:
+//= require lazy/map_search
 
