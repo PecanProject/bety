2de9db5ca6adca0efff75e9808fe5db7ec422b89
Scott Rohde <srohde@igb.illinois.edu>
2016-03-01 11:52:58 -0600

    Split out jQuery files from main JavaScript assets manifest to solve a problem with bad interactions between jQuery and Prototype.  Added the new jQuery-specific manifest to the assets precompile list and added a line to include it in the application layout.
    Added bulk_upload.css to the assets precompilation list since it isn't in the
    stylesheet manifest.

---
+++ app/assets/javascripts/jquery-compilation.js
@@ -0,0 +1,14 @@
+// require jquery
+// require jquery_ujs
+// require jquery-ui
+
+// The above three lines might be able to replace the following 5.
+// TO-DO: Replace use jquery-rails and jquery-ui-rails gems in place of the
+// files in the lazy/ and libs/ directories.
+
+//= require lazy/jquery-1.11.0
+//= require libs/jquery-ui-1.10.4.custom/js/jquery-ui-1.10.4.custom.js
+//= require libs/jquery-migrate-1.2.1
+
+//= require lazy/jquery-1.7.2
+//= require lazy/jquery-ui-1.10.4.min
--- config/application.rb
+++ config/application.rb
@@ -70,6 +70,6 @@ module BetyRails3
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.precompile += %w( lazy/feedback.js lazy/bulk_upload.js lazy/autocomplete.js mylibs/maps.js )
+    config.assets.precompile += %w( jquery-compilation.js lazy/feedback.js lazy/bulk_upload.js lazy/autocomplete.js mylibs/maps.js bulk_upload.css )
   end
 end
