aa5289830b445a18d75855b606dd25645e9e9e57
Max Burnette <mburnet88@gmail.com>
2016-03-15 11:45:36 -0500

    Added files from betydb_4.5 zip file & moved gem install into Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,22 +3,30 @@ MAINTAINER Max Burnette <mburnet2@illinois.edu>
 
 EXPOSE 3000
 
+
 # Install dependencies
 RUN apt-get update &&\
     apt-get install -y sudo curl unzip postgresql-client &&\
-    /bin/mkdir /home/bety &&\
-    useradd -s /bin/bash postgres &&\
+    /bin/mkdir /home/bety
+
+COPY / /home/bety
+
+RUN useradd -s /bin/bash postgres &&\
     /bin/chown -R postgres /home/bety
 
 # Create postgres user (which will play nice with postgres container)
 
 # Move the build file & bety config out to remote server
-COPY docker/entrypoint.sh /home/bety/
-COPY docker/database.yml /home/bety/
+# COPY docker/entrypoint.sh /home/bety/
+# COPY docker/database.yml /home/bety/
+
+RUN cd /home/bety &&\
+    /bin/sed -i "/capybara-webkit/ s/^/# /" Gemfile &&\
+    gem install bundler &&\
+    bundle install
 
 # Change permissions for files/folders & change to user postgres
-# RUN chown -R postgres /home/bety
 USER postgres
 
-ENTRYPOINT ["/home/bety/entrypoint.sh"]
+ENTRYPOINT ["/home/bety/docker/entrypoint.sh"]
 CMD ["bety"]
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,19 +5,19 @@ if [ "$1" = 'bety' ]; then
     BETY_VERSION="betydb_4.5"
 
     # Download BETY zip file from pecan archive
-    cd home/bety
-    curl -LOk https://github.com/PecanProject/bety/archive/${BETY_VERSION}.zip
-    unzip ${BETY_VERSION}.zip
-    cd bety-${BETY_VERSION}
+    cd /home/bety
+    #curl -LOk https://github.com/PecanProject/bety/archive/${BETY_VERSION}.zip
+    #unzip ${BETY_VERSION}.zip
+    #cd bety-${BETY_VERSION}
 
     # Comment out capybara-webkit line and install Rails dependencies
-    /bin/sed -i "/capybara-webkit/ s/^/# /" Gemfile
-    gem install bundler
-    bundle install
+    #/bin/sed -i "/capybara-webkit/ s/^/# /" Gemfile
+    #gem install bundler
+    #bundle install
 
     # Move database config file into config directory
     cd config
-    /bin/cp /home/bety/database.yml database.yml
+    /bin/cp /home/bety/docker/database.yml database.yml
     /bin/sed -i "/host:/ s/$/ $PG_PORT_5432_TCP_ADDR/" database.yml
     /bin/sed -i "/port:/ s/$/ $PG_PORT_5432_TCP_PORT/" database.yml
     # /bin/cp application.yml.template application.yml
