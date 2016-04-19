8fb675f66b8b5a67c01e198399b314f4c810774e
Scott Rohde <srohde@igb.illinois.edu>
2016-04-19 15:26:04 -0500

    Fixed Ensemble integration spec.

--- spec/features/ensemble_integration_spec.rb
+++ spec/features/ensemble_integration_spec.rb
@@ -12,12 +12,14 @@ feature 'Ensembles work' do
       expect(page).to have_content 'Listing Ensembles'
     end
 
+    # to-do: move this to a routing spec
     it 'should not have "New Ensemble" page ' do
-      expect { visit '/ensembles/new' }.to raise_error
+      expect { visit '/ensembles/new' }.to raise_error AbstractController::ActionNotFound
     end
 
+    # to-do: move this to a routing spec
     it 'should not have "Edit Ensemble" page ' do
-      expect { visit '/ensembles/edit/1' }.to raise_error
+      expect { visit '/ensembles/edit/1' }.to raise_error ActionController::RoutingError
     end
 
     context 'clicking view ensemble button' do
