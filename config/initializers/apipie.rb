45cf409631ab5e0036c9903045f86978b31d05e6
Scott Rohde <srohde@igb.illinois.edu>
2016-04-27 10:06:38 -0500

    Changed Api documentation location from /apipie to /api/docs.

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,7 +2,7 @@ Apipie.configure do |config|
   config.app_name                = "BETYdb"
   config.default_version         = "beta"
   config.api_base_url            = ""
-  config.doc_base_url            = "/apipie"
+  config.doc_base_url            = "/api/docs"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/beta/*.rb"
 
