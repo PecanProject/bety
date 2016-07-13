5f6d763223f97e5d758a3fd99d1009171f29ba8e
Scott Rohde <srohde@igb.illinois.edu>
2016-07-13 14:39:37 -0500

    Amended fix for https://github.com/PecanProject/bety/issues/429: We seem to need two separate routes to match both '/api' and everything under it.

--- config/routes.rb
+++ config/routes.rb
@@ -257,6 +257,7 @@ BetyRails3::Application.routes.draw do # RAILS3 |map| removed
     end
   end
   match '/api', controller: 'api/base', action: 'bad_url'
+  match '/api/*remainder', controller: 'api/base', action: 'bad_url'
 
   get '/application/use_citation/:id', controller: 'application', action: 'use_citation'
   get '/application/remove_citation'
