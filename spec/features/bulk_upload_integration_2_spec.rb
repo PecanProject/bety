158a22d1911ad47eefe647ad568caf6c81cd4e06
Scott Rohde <srohde@igb.illinois.edu>
2017-06-22 17:46:04 -0500

    Removed pending from the test that checks that the implementation of the feature in issue #459 is working.
    Also tweaked the test to get it working properly.

--- spec/features/bulk_upload_integration_2_spec.rb
+++ spec/features/bulk_upload_integration_2_spec.rb
@@ -542,7 +542,6 @@ CSV
     describe "How parsing of variables in the heading works" do
 
       specify "A variable in the heading should be recognized even if it isn't in the trait_covariate_associations table" do
-        pending "A working implementation for the feature in issue #459"
         visit '/bulk_upload/start_upload'
         attach_file 'CSV file', Rails.root.join('spec',
                                                 'fixtures',
@@ -554,10 +553,13 @@ CSV
         expect(page).not_to have_content('error')
         click_link 'Specify'
         click_button 'Confirm'
-        expect { click_button 'Insert Data' }.to change { Trait.count }.by(2)
-                                             .and change { Covariate.count }.by(2)
-                                             .and change { Entity.count }.by 1;     # The two rows in the file have the same entity name.
-        expect(first("div.alert-success")).to have_content("Data from valid-test-data-with-traits-not-in-associations-table.csv was successfully uploaded.")
+        expect {
+          click_button 'Insert Data'
+          # Use this instead of sleep():
+          expect(first("div.alert-success")).to have_content("Data from valid-test-data-with-traits-not-in-associations-table.csv was successfully uploaded.")
+        }.to change { Trait.count }.by(2)
+               .and change { Covariate.count }.by(0)
+                      .and change { Entity.count }.by 1;     # The two rows in the file have the same entity name.
       end
 
       specify "If there is a covariate variable in the heading without there being a corresponding trait variable, the CSV file is invalid" do
