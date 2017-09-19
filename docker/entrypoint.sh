8681d1826ba5e38bed5783962433201a4ac74fa5
Rob Kooper <kooper@illinois.edu>
2017-09-19 00:27:37 -0500

    Cleanup of docker
    - cleanup of entrypoint, added more commands
    - dockerfile cleanup, exposed database variables
    - docker-compose.yml is now version 3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
 
-if [ "$1" = 'bety' ]; then
-    # Wait for Postgres to start
-    # http://www.onegeek.com.au/articles/waiting-for-dependencies-in-docker-compose
-    WAIT=0
-    while ! nc -z $PG_PORT_5432_TCP_ADDR $PG_PORT_5432_TCP_PORT; do
-      sleep 1
-      WAIT=$(($WAIT + 1))
-      if [ "$WAIT" -gt 15 ]; then
-        echo "Error: Timeout wating for Postgres to start"
-        exit 1
-      fi
-    done
+# Wait for Postgres to start
+# http://www.onegeek.com.au/articles/waiting-for-dependencies-in-docker-compose
+WAIT=0
+while ! nc -z postgresql 5432; do
+  sleep 1
+  WAIT=$(($WAIT + 1))
+  if [ "$WAIT" -gt 15 ]; then
+    echo "Error: Timeout wating for Postgres to start"
+    exit 1
+  fi
+done
 
-    # Move database config file into config directory & add host/port
-    cd /home/bety/config
-    /bin/cp /home/bety/docker/database.yml database.yml
-    /bin/sed -i "/host:/ s/$/ $PG_PORT_5432_TCP_ADDR/" database.yml
-    /bin/sed -i "/port:/ s/$/ $PG_PORT_5432_TCP_PORT/" database.yml
+# configure ruby app
+/bin/sed -e "/username:/ s/bety$/${OWNER}/" \
+         -e "/password:/ s/bety$/${PASSWORD}/" \
+         -e "/database:/ s/bety$/${DATABASE}/" \
+         /home/bety/docker/database.yml > /home/bety/config/database.yml
+/bin/sed -e '/serve_static_assets/ s/false$/true/' \
+         -i config/environments/production.rb
 
-    # Create bety database if it does not exist
-    if ! psql -h $PG_PORT_5432_TCP_ADDR -p $PG_PORT_5432_TCP_PORT -U postgres -lqt | cut -d \| -f 1 | grep -w "bety" > /dev/null ; then
-      echo "Creating bety database"
-      createdb -U postgres -h $PG_PORT_5432_TCP_ADDR -p $PG_PORT_5432_TCP_PORT bety
-    fi
-
-    # Download & initialize Bety database contents
-    cd ../script
-    curl -LOs https://raw.githubusercontent.com/PecanProject/pecan/master/scripts/load.bety.sh
-    chmod +x load.bety.sh
-    ./load.bety.sh -a "postgres" -p "-h $PG_PORT_5432_TCP_ADDR -p $PG_PORT_5432_TCP_PORT" -o postgres -c -u -g -r 0 -m 99
-
-    cd ..
-    rake db:migrate
-    rails s
-else
-    exec "$@"
-fi
+# start right job
+case $1 in
+    "initialize" )
+        SERVER=$( echo ${REMOTE_SERVERS} | awk '{print $1}' )
+        echo "Create new database, initialized from server ${SERVER}"
+        psql -h postgresql -p 5432 -U postgres -c "CREATE ROLE ${OWNER} WITH LOGIN CREATEDB NOSUPERUSER NOCREATEROLE PASSWORD '${PASSWORD}'"
+        psql -h postgresql -p 5432 -U postgres -c "CREATE DATABASE ${DATABASE} WITH OWNER ${OWNER}"
+        ./load.bety.sh -a "postgres" -d "${DATABASE}" -p "-h postgresql -p 5432" -o ${OWNER} -c -u -g -m ${LOCAL_SERVER} -r ${SERVER}
+        ;;
+    "sync" )
+        echo "Synchronize with servers ${REMOTE_SERVERS}"
+        for r in ${REMOTE_SERVERS}; do
+            ./load.bety.sh -a "postgres" -d "${DATABASE}" -p "-h postgresql -p 5432" -o ${OWNER} -r ${r}
+        done
+        ;;
+    "dump" )
+        echo "Dump data from server ${LOCAL_SERVER}"
+        ./dump.bety.sh -d "${DATABASE}" -p "-h postgresql -p 5432 -U postgres" -m ${LOCAL_SERVER} -o dump
+        ;;
+    "migrate" )
+        echo "Migrate databae."
+        rake db:migrate SKIP_SCHEMASPY=YES
+        ;;
+    "server" )
+        echo "Start running BETY"
+        exec rails s
+        ;;
+    "help" )
+        echo "initialize : create a new database and initialize with data from server 0"
+        echo "sync       : synchronize database with remote servers ${REMOTE_SERVERS}"
+        echo "dump       : dumps local database"
+        echo "migrate    : migrates the database to a new version of bety"
+        echo "server     : runs the server"
+        echo "help       : this text"
+        ;;
+    * )
+        exec "$@"
+esac
