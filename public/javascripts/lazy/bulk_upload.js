74a5b8eca5b9af87d1cb30bcd979ffa9242febd2
Scott Rohde <srohde@igb.illinois.edu>
2017-12-14 16:41:20 -0600

    Implementation of assigning methods to traits in Bulk Upload Wizard (GH issue #545).
    Changes:
    
    .yardopts - change to using markdown in place of RDoc markup
    
    application_controller.rb
    
      Fleshed out documentation of #search_model.  Eliminated "else" branch in
      #search_model: let the caller decide what to do if there are no results for
      search terms of size 2 or longer.
    
    bulk_upload_controller.rb
    
      Extensive documentation revisions, fleshing it out and converting to using
      markdown-style comments.
    
      #clear_session_data - now clears out trait_to_method_mapping from session
    
      Added new #store_trait_method_mapping_in_session action to be called by jQuery
      when user selects a method.
    
      #choose_global_data_values - This action now sets instance variable
       @heading_variable_info, which the view needs to determine what method widgets
       to create.  It also initializes session key :trait_to_method_mapping if it
       doesn't exist.
    
      #confirm_data - This action now sets instance variable
       @variable_to_covariates_mapping, which the view uses to show the user which
       covariates are associated with each trait variable.
    
    methods_controller.rb
    
      Added #bu_autocomplete action as a source for the jQuery UI autocompletion
      widgets for method selection.  Set coding to utf-8 since comments include
      non-ascii.
    
    application_helper.rb
    
      Minor comment revisions.
    
    bulk_upload_helper.rb
    
      Added #method_autocompletion_field for easy production of autocompletion
      widget for selecting a method.
    
    bulk_upload_data_set.rb
    
      Extensive additions and changes to documentation comments.
    
      Made heading_variable_info an attribute so bulk_upload_controller can access
      it directly.  Made #trait_data? and #get_variables_in_heading public for the
      same reason.
    
      Most importantly, we now set the method_id attribute in
      #add_trait_specific_attributes.
    
    choose_global_data_values.html.erb
    
      Set the JavaScript variable bu_completion_fields in the "content_for" block.
    
      Added a section for choosing methods to go with each trait.
    
    confirm_data.html.erb
    
      Always use "Specify Dataset-wide values" as the back-link name if it is a
      trait-data upload.
    
      Added a section showing the covariates and methods associated with each trait.
    
    display_csv_file.html.erb
    
      Always use "Specify Dataset-wide values" as the forward-link name if it is a
      trait-data upload.
    
    routes.rb
    
      Added a route for /bulk_upload/store_trait_method_mapping_in_session.
    
    bulk_upload.js
    
      Added to the set of actions to be done when the document on the "Choose Global
      Data Values" page is ready, including:
    
         Converting each element whose id is a key of bu_completion_fields into an
         autocompletion widget.
    
         Adding a handler for the form-submit event so that submission can be
         blocked if any of the method-choice fields is invalid.
    
      Defined handlers for the select, change, and click events for the method
      autocompletion fields.

--- public/javascripts/lazy/bulk_upload.js
+++ public/javascripts/lazy/bulk_upload.js
@@ -56,4 +56,85 @@ jQuery(function() {
         jQuery('#global_values_citation_id').val(ui.item.value);
     });
 
+    jQuery("form").submit(function(event, ui) {
+        var allowSubmission = true;
+
+        for (autocompletion_widget_id in bu_completion_fields) {
+            allowSubmission = allowSubmission && bu_completion_fields[autocompletion_widget_id]["valid"];
+        }
+
+        return allowSubmission;
+    });
+
+    for (autocompletion_widget_id in bu_completion_fields) {
+        jQuery('#' + autocompletion_widget_id).autocomplete({
+            minLength: 0,
+            source: ROOT_URL + "/methods/bu_autocomplete.json",
+            select: create_select_function(autocompletion_widget_id),
+            focus: function(event, ui) {
+                // Prevent the value (the id number) of an item selected from
+                // displaying when using the arrow keys:
+                event.preventDefault();
+            },
+            change: create_change_function(autocompletion_widget_id)
+        });
+        jQuery('#' + autocompletion_widget_id).click(create_click_function(autocompletion_widget_id));
+
+    }
+
 });
+
+function create_select_function(autocompletion_widget_id) {
+    return function(event, ui) {
+        // Prevent the value (the id number) of the item selected from
+        // displaying after a selection has been made ...
+        event.preventDefault();
+
+        // ... and display the *label* of the item selected instead ...
+        var display = ui.item.label;
+        // ... unless it's "[no value]"; then just display the placeholder.
+        if (ui.item.label == "[no value]") {
+            display = "";
+        }
+        jQuery(this).val(display);
+
+        // Clear the error message if present:
+        jQuery('label[for = "' + autocompletion_widget_id + '"] span.error').hide();
+
+        // Save the selected values in the session, both for making
+        // values "sticky" and for use by subsequent pages of the
+        // wizard:
+        jQuery.post("/bulk_upload/store_trait_method_mapping_in_session",
+                    {
+                        "trait_name": event.target.id,
+                        "method_info": {
+                            "label": display,
+                            "value": ui.item.value
+                        }
+                    });
+
+    }
+}
+
+function create_change_function(autocompletion_widget_id) {
+    return function(event, ui) {
+        var validValue = (ui.item != null && ui.item.label != null);
+
+        if (validValue) {
+            jQuery('label[for = "' + autocompletion_widget_id + '"] span.error').hide();
+        }
+        else {
+            jQuery('label[for = "' + autocompletion_widget_id + '"] span.error').show();
+
+            jQuery(this).select();
+        }
+        bu_completion_fields[autocompletion_widget_id]["valid"] = validValue;
+    }
+}
+
+function create_click_function(autocompletion_widget_id) {
+    return function(event, ui) {
+        jQuery('#' + autocompletion_widget_id).select();
+        jQuery('#' + autocompletion_widget_id).autocomplete("search");
+    }
+}
