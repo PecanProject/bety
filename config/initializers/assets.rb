4c3411edfff5abbece0bb482d621c501b1f86779
Scott Rohde <srohde@igb.illinois.edu>
2018-04-19 16:17:51 -0500

    Modified some initializers to fix problems discovered during testing.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
   bulk_upload.css
+  schemaSpy.css
 
   lazy/login.js
   lazy/map_search.js
