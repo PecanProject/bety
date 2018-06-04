3214b984cdfceb3c20eeea218d526d40a133df3f
Scott Rohde <srohde@igb.illinois.edu>
2018-06-04 16:27:28 -0500

    Modified output for man option.

--- script/db_maintenance/fix_database.rb
+++ script/db_maintenance/fix_database.rb
@@ -1,33 +1,6 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
-# This script generates a file of SQL statements to fix the sequence objects and
-# public table ids for a specified BETYdb database.  It assumes id numbers were
-# misallocated in the 99 billion range and that rows having ids in that range
-# should be reassigned an id number in the range appropriate to the machine
-# specified by the user.
-#
-# Database connection information may either be specified in a YAML file
-# "connection_information.yml" (see "connection_information.yml-sample" for an
-# example) or prompted for interactively.  The user should also specify the
-# number of the target machine and the name of the file to write the SQL
-# statements to.
-#
-# Steps for using the script are as follows:
-#
-# 1. Take BETYdb instance that uses the target database off line.
-#
-# 2. Run the script.
-#
-# 3. Check that there are appropriate foreign-key constraints on all tables that
-#    refer to any table mentioned in any of the generated UPDATE statements.
-#    These foreign-key constraints should use the "ON UPDATE CASCADE" clause.
-#
-# 4. If the necessary constraints are present, run the SQL statements in the generated file.
-#
-# 5. Restore the BETYdb instance that was taken off line in step 1.
-#
-
 USAGE_DETAILS = <<'EOS'
 
 DESCRIPTION
@@ -43,20 +16,31 @@ Database connection information may either be specified in a YAML file
 example) or prompted for interactively.  The user should also specify the number
 of the target machine and the name of the file to write the SQL statements to.
 
+HOW TO USE
+
 Steps for using the script are as follows:
 
 1. Take BETYdb instance that uses the target database off line.
 
-2. Run the script.
+2. (Optional) Write a YAML file called connection_information.yml in the same
+   directory where this script resides that contains database connection
+   information, the id number of the machine using the database, and the name of
+   a file to write SQL statements to.  See the CONNECTION SPECIFICATION and
+   OTHER SCRIPT PARAMETERS sections below.  You may use
+   connection_information.yml-sample as a model.
 
-3. Check that there are appropriate foreign-key constraints on all tables that
+3. Run the script.
+
+4. Check that there are appropriate foreign-key constraints on all tables that
    refer to any table mentioned in any of the generated UPDATE statements.
    These foreign-key constraints should use the \"ON UPDATE CASCADE\" clause.
+   You may use the \"find_missing_constraints.rb\" script to check what
+   foreign-key constraints may be missing.
 
-4. If the necessary constraints are present, run the SQL statements in the
+5. If the necessary constraints are present, run the SQL statements in the
    generated file.
 
-5. Restore the BETYdb instance that was taken off line in step 1.
+6. Restore the BETYdb instance that was taken off line in step 1.
 
 
 CONNECTION SPECIFICATION
@@ -87,7 +71,7 @@ In addition to database connection parameters, the user must provide:
 
 These may be specified as top-level keys in the YAML file.  Otherwise, they are
 prompted for.
-  
+ 
 EOS
 
 ## Parse command line
