3633d39a097021da061aa246f185c81a15e613de
Scott Rohde <srohde@igb.illinois.edu>
2018-11-26 15:23:10 -0600

    Implement feature requested in Issue #605: allow key-less API access to public data.

--- app/services/api_authentication_system.rb
+++ app/services/api_authentication_system.rb
@@ -3,7 +3,11 @@ module ApiAuthenticationSystem
 
   # Override default access_denied action.
   def access_denied
-    @errors = "authentication failed"
+    if @errors
+      @errors = "authentication failed: " + @errors
+    else
+      @errors = "authentication failed"
+    end
     render status: 401
   end
 
@@ -17,6 +21,23 @@ module ApiAuthenticationSystem
     else
       false
     end
+  end
+
+  # Override "login_from_api_key" so that if no key is given or the given key is
+  # invalid, the user is logged in as the guest user.
+  def login_from_api_key
+    key = params[:key]
+    if key.nil?
+      u = User.find_by_login('guestuser')
+    else
+      u = User.find_by_apikey(params[:key]) || User.find_by_login('guestuser')
+    end
+
+    if u.nil?
+      @errors = "You must either use a valid API key or set up the guest user account."
+    end
+
+    return u
   end
 
 end
