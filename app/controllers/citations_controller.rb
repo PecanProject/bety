e55b2cb3176e784c1cecc25955b2f9b9baaf9c69
Scott Rohde <srohde@igb.illinois.edu>
2019-08-09 11:24:31 -0500

    Make the citation controller's "show" action throw an ActiveRecord::RecordNotFound exception when the given model is not found, just as all the other controllers do.
    This fixes issue #668.

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -123,7 +123,10 @@ class CitationsController < ApplicationController
   # GET /citations/1
   # GET /citations/1.xml
   def show
-    @citation = Citation.where(:id => params[:id]).includes(params[:include]).first
+    # find_by! throws an ActiveRecord::RecordNotFound exception if no citation
+    # with the given id exists so that we don't attempt to display a nil
+    # citation.
+    @citation = Citation.includes(params[:include]).find_by!(:id => params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
