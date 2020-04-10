4f4a7b452caf65dd1d80e4c8318e8ef59e8ba69e
Rob Kooper <kooper@illinois.edu>
2020-04-10 14:26:47 -0500

    dumping DB + bug fix (#695)
    * script to dump database
    * docker push + cron
    * only push to dockerhub if username/password set
    * only run on PecanProject/bety
    * script to easily add users to BETY
    * make sure to use environment variable for secret
    
    The environment variable SECRET_KEY_BASE was ignored. This fixes #696.
    If you had SECRET_KEY_BASE set, please unset it now, otherwise you can no longer login.
    load-bety.sh script now uses script/betyuser.sh to add users with correct password.

--- initdb/initdb.sh
+++ initdb/initdb.sh
@@ -6,3 +6,11 @@ psql -h postgres -p 5432 -U postgres -c "CREATE ROLE bety WITH LOGIN CREATEDB NO
 
 # load database from dump
 pg_restore -c -C -v -h postgres -U postgres -d postgres -F c /db.dump
+
+# print some hints on what to do next
+echo ""
+echo "To fix the database id to be 77 instead of the default of 99:"
+echo "docker-compose run -e LOCAL_SERVER=77 bety fix"
+echo ""
+echo "To add a user, you can use:"
+echo "docker-compose run bety user 'login' 'password' 'full name' 'email' 1 1"
